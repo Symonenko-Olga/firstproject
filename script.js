72eb12543144228566d6ebfc3a3cccb6b829c6f9
Olga <olhasymon@gmail.com>
2024-08-24 18:32:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+/* alert('Всем привет!!!');
+alert('Я JavaScript');
+
+
+let massage = 'Olga', age = 34, hobby = 'JS';
+alert(massage+age+hobby);
+
+// неизменяемая переменная const
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+alert(COLOR_RED);
+
+*/
+
+//Функция prompt принимает два аргумента:
+
+let meno = prompt('Как тебя зовут?', 'Введите имя');
+alert(`Меня зовут ${meno}`);
+
+// Функция confirm отображает модальное окно с текстом вопроса question и двумя кнопками: OK и Отмена.
+
+let ques = confirm('Ты лучше всех?');
+alert(ques);
+
